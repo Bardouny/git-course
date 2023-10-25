7ebb06e50feb80c21696ee179ad5fb3cdbd604cc
Hossam Bardouny <hbardouny@gmail.com>
2023-10-25 21:49:56 +0200

    valid

--- formValidation.js
+++ formValidation.js
@@ -3,4 +3,5 @@ console.log('hello on pull');
 //.....form....//
 document.querySelector('#inp').innerHTML = 'Abslam';
 console.log('jorjin');
-//./...///
+//...........ADDED by valid........//
+console.log('valid');
